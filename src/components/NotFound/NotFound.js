79422a4e129123bfd6b0c3daece102f79979eb47
ChandraMahanthi <mahanthichandra9640@gmail.com>
2024-11-11 22:21:58 +0530

    Application created

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='not-found my-5'>
+      <div className='container flex align-center justify-center'>
+        We're sorry there are no items right now, but our best minds are woking on it so we'll get it done
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
